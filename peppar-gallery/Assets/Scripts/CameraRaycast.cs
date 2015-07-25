70dd82187af2617089a60be9ee9d64cfc14a1334
autolame <autolame@yahoo.de>
2015-07-25 13:11:38 +0200

    added cursor interaction dummy

--- peppar-gallery/Assets/Scripts/CameraRaycast.cs
+++ peppar-gallery/Assets/Scripts/CameraRaycast.cs
@@ -6,6 +6,7 @@ public class CameraRaycast : MonoBehaviour
 
 
     Ray _ray;
+    RaycastHit hit;
     float distanceToGround = 0;
 
     [SerializeField]
@@ -14,11 +15,22 @@ public class CameraRaycast : MonoBehaviour
     [SerializeField]
     public WorldObjectManager WOM;
 
+    [SerializeField]
+    float _startTime;
+
+    float _time;
+
+    [SerializeField]
+    GameObject _crosshair;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-
+        if (_startTime == 0)
+        {
+            _startTime = 3;
+        }
+        _time = _startTime;
     }
 
     // Update is called once per frame
@@ -27,28 +39,66 @@ public class CameraRaycast : MonoBehaviour
         _ray.origin = transform.position;
         _ray.direction = transform.forward;
 
-        RaycastHit hit;
 
 
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        //{
+        //    Debug.Log(" mouse Hit:" + hit.transform.gameObject);
+        //}
+
+        ScaleCursor();
+    }
+
+    void ScaleCursor()
+    {
         if (Physics.Raycast(_ray, out hit, Mathf.Infinity))
         {
-            hitGO = hit.transform.gameObject;
-            WOM = hitGO.transform.parent.parent.GetComponent<WorldObjectManager>();
+
+            _time -= Time.deltaTime;
+
+            Debug.Log(_time);
+            if (_time <= 0)
+            {
+                RotateObject();
+            }
+
+            var scale = _crosshair.transform.localScale;
+
+            scale.x -= Time.deltaTime;
+            scale.y -= Time.deltaTime;
+            scale.z -= Time.deltaTime;
+
+            scale = new Vector3(scale.x, scale.y, scale.z);
+
+            //scale = new Vector3(Mathf.MoveTowards(scale.x, 0, .05f), Mathf.MoveTowards(scale.y, 0, .05f), Mathf.MoveTowards(scale.z, 0, .05f));
+            //scale = new Vector3(scale.x );
 
 
-            Debug.Log("eye hit:" + hit.transform.gameObject);
-            WOM.GotHit = true;
+            _crosshair.transform.localScale = scale;
+
+
+
         }
         else
         {
+            _time = _startTime;
+            _crosshair.transform.localScale = Vector3.one;
             WOM.GotHit = false;
         }
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, Mathf.Infinity))
-        {
-            Debug.Log(" mouse Hit something");
-        }
+    }
+
+    void RotateObject()
+    {
+        //if (Physics.Raycast(_ray, out hit, Mathf.Infinity))
+        //{
+        Debug.Log("rotating");
+        hitGO = hit.transform.gameObject;
+        WOM = hitGO.transform.parent.parent.GetComponent<WorldObjectManager>();
+
+        Debug.Log("eye hit:" + hit.transform.gameObject);
+        WOM.GotHit = true;
     }
 
 
